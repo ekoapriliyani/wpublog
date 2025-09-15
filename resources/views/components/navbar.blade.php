d4aecfea3b27297e72d28f7bc3c9067876411345
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-09-15 10:38:00 +0700

    add Dashboard Controller

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -85,33 +85,42 @@
             <el-disclosure id="mobile-menu" hidden class="block md:hidden">
                 <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                    <x-nav-link class="block" href="/" :current="request()->is('/')">Home</x-nav-link>
-                    <x-nav-link class="block" href="/posts" :current="request()->is('posts')">Blog</x-nav-link>
-                    <x-nav-link class="block" href="/about" :current="request()->is('about')">About</x-nav-link>
-                    <x-nav-link class="block" href="/contact" :current="request()->is('contact')">Contact</x-nav-link>
+                    <x-my-nav-link class="block" href="/" :current="request()->is('/')">Home</x-my-nav-link>
+                    <x-my-nav-link class="block" href="/posts" :current="request()->is('posts')">Blog</x-my-nav-link>
+                    <x-my-nav-link class="block" href="/about" :current="request()->is('about')">About</x-my-nav-link>
+                    <x-my-nav-link class="block" href="/contact" :current="request()->is('contact')">Contact</x-my-nav-link>
                 </div>
                 <div class="border-t border-white/10 pt-4 pb-3">
-                    <div class="flex items-center px-5">
-                        <div class="shrink-0">
-                            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt=""
-                                class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
+                    @if (Auth::check())
+                        <div class="flex items-center px-5">
+                            <div class="shrink-0">
+                                <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                    alt="{{ Auth::user()->name }}"
+                                    class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
+                            </div>
+                            <div class="ml-3">
+                                <div class="text-base/5 font-medium text-white">{{ Auth::user()->name }}</div>
+                            </div>
                         </div>
-                        <div class="ml-3">
-                            <div class="text-base/5 font-medium text-white">Tom Cook</div>
-                            <div class="text-sm font-medium text-gray-400">tom@example.com</div>
+                        <div class="mt-3 space-y-1 px-2">
+                            <a href="/profile"
+                                class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Your
+                                profile</a>
+                            <a href="/dashboard"
+                                class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Dashboard</a>
+                            <form method="POST" action="/logout">
+                                @csrf
+                                <button type="submit" href="#"
+                                    class="block w-full text-start rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white cursor-pointer">Log
+                                    Out</button>
+                            </form>
                         </div>
-                    </div>
-                    <div class="mt-3 space-y-1 px-2">
-                        <a href="#"
-                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Your
-                            profile</a>
-                        <a href="#"
-                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Settings</a>
-                        <a href="#"
-                            class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Sign
-                            out</a>
-                    </div>
+                    @else
+                        <div class="my-3 space-y-1 px-2">
+                            <a href="/login" class="text-white text-sm font-medium block py-2">Login</a>
+                            <a href="/register" class="text-white text-sm font-medium block py-2">Register</a>
+                        </div>
+                    @endif
                 </div>
             </el-disclosure>
         </nav>
