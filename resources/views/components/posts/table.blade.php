446a2643a3584ed27b99c74873d5c6eff1ce0998
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-09-17 14:14:56 +0700

    searching

--- resources/views/components/posts/table.blade.php
+++ resources/views/components/posts/table.blade.php
@@ -1,12 +1,13 @@
 <!-- Start block -->
 <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 antialiased">
+    <h2 class="text-xl font-semibold pl-4 mb-4">Post by : {{ Auth::user()->name }}</h2>
     <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
         <!-- Start coding here -->
         <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                 <div class="w-full md:w-1/2">
-                    <form class="flex items-center">
-                        <label for="simple-search" class="sr-only">Search</label>
+                    <form class="flex items-center" action="" method="GET">
+                        <label for="simple-search" class="sr-only">Search Post</label>
                         <div class="relative w-full">
                             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                 <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
@@ -16,9 +17,9 @@
                                         clip-rule="evenodd" />
                                 </svg>
                             </div>
-                            <input type="text" id="simple-search"
+                            <input type="text" id="simple-search" name="keyword"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="Search" required="">
+                                placeholder="Search Post">
                         </div>
                     </form>
                 </div>
@@ -60,7 +61,6 @@
                         <tr>
                             <th scope="col" class="px-4 py-4">No</th>
                             <th scope="col" class="px-4 py-3">Title</th>
-                            <th scope="col" class="px-4 py-3">Author</th>
                             <th scope="col" class="px-4 py-3">Category</th>
                             <th scope="col" class="px-4 py-3">Created At</th>
                             <th scope="col" class="px-4 py-3">
@@ -76,7 +76,6 @@
                                     {{ $loop->iteration }}
                                 </th>
                                 <td class="px-4 py-3">{{ $post->title }}</td>
-                                <td class="px-4 py-3">{{ $post->author->name }}</td>
                                 <td class="px-4 py-3">{{ $post->category->name }}</td>
                                 <td class="px-4 py-3">{{ $post->created_at->diffForHumans() }}</td>
                                 <td class="px-4 py-3 flex items-center justify-end">
@@ -142,9 +141,11 @@
                     </tbody>
                 </table>
             </div>
-            <div class="p-3">
-                {{ $posts->links() }}
-            </div>
+            @if ($posts->hasPages())
+                <div class="p-3">
+                    {{ $posts->links() }}
+                </div>
+            @endif
         </div>
     </div>
 </section>
